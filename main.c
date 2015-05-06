4fbf0ddafff50f9ad47c4c2e10f60789b9061132
mnmoretti <manuel.moretti@tiscali.it>
2015-05-06 20:34:50 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// Definisci le costanti
-#define N 3
-#define MAX_STRLEN 30
-#define FNAME "elenco.csv"
-
-// Definisci il tipo di dato struct s_studente
-struct s_studente {
-    char nome[MAX_STRLEN+1];
-    char cognome[MAX_STRLEN+1];
-    int eta;
-    char classe[MAX_STRLEN+1];
-};
-
-// Definisci studente come struct s_studente
-typedef struct s_studente studente;
-
-int main(int argc, char** argv) {
-    
-    return (EXIT_SUCCESS);
-}
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#define N 3 
+#define MAX_STRLEN 30 
+#define FNAME "elenco.csv" 
+struct s_studente { 
+     char nome[MAX_STRLEN+1]; 
+     char cognome[MAX_STRLEN+1]; 
+     int eta; 
+     char classe[MAX_STRLEN+1]; 
+ }; 
+ 
+ typedef struct s_studente studente; 
+ 
+ int main(int argc, char** argv) { 
+     FILE *outFile; 
+     studente alunno; 
+     int i; 
+      
+     outFile = fopen(FNAME, "w"); 
+     if(outFile){ 
+        
+         fprintf(outFile, "nome;cognome;eta';classe\n"); 
+ 
+         for(i=0;i<N;i++) { 
+            
+             printf("Inserisci il nome: "); 
+             scanf("%s", alunno.nome); 
+             fprintf(outFile, "%s;", alunno.nome); 
+ 
+             printf("Inserisci il cognome: "); 
+             scanf("%s", alunno.cognome); 
+             fprintf(outFile, "%s;", alunno.cognome); 
+ 
+             printf("Inserisci l'eta': "); 
+             scanf("%d", &alunno.eta); 
+             fprintf(outFile, "%d;", alunno.eta); 
+ 
+             printf("Inserisci la classe: "); 
+             scanf("%s", alunno.classe); 
+             fprintf(outFile, "%s\n", alunno.classe); 
+             printf("\n"); 
+         } 
+ 
+         fclose(outFile); 
+     }else { 
+        
+         printf("errore nella creazione del file"); 
+     } 
+      
+     return (EXIT_SUCCESS); 
+ } 
